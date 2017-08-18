74e4408bcec151a1688110363fe17f8c1eb52341
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-08-18 08:38:59 -0700

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    method: 'GET',
+    path: '/public',
+    config: {
+      auth: false,
+      handler: function (request, reply) {
+        return reply(request.auth);
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/private',
+    config: {
+      handler: function(request, reply) {
+        return reply(request.auth);
+      }
+    }
+  }
+]
---
+++ app/server.js
@@ -0,0 +1,24 @@
+const Hapi = require('hapi');
+const Basic = require('hapi-auth-basic');
+const Blipp = require('blipp');
+const routes = require('./routes');
+
+const server = new Hapi.Server();
+server.connection({ port: 9000 });
+server.register([
+  Basic,
+  { register: Blipp, options: { showAuth: true } }
+], (err) => {
+  const basicConfig = {
+    validateFunc: function (request, username, password, callback) {
+      if (username !== 'admin' || password !== 'password') {
+        return callback(null, false);
+      }
+      return callback(null, true, { username: 'admin' });
+    }
+  };
+  server.auth.strategy('simple', 'basic', basicConfig);
+  server.auth.default('simple');
+  server.route(routes);
+  server.start(() => {});
+});
